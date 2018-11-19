a1cbe3d5b7d6167697dcc9a343be3611937b3bd2
samane658 <42388358+samane658@users.noreply.github.com>
2018-11-20 02:08:23 +0330

    connectionString

--- AttendanceApp/Models/IdentityModels.cs
+++ AttendanceApp/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace AttendanceApp.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("AppDbConnection", throwIfV1Schema: false)
         {
         }
 
